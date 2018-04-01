ee89fc1b6ffd479e5256ed1fdfdfa8aa20623d6a
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-04-01 10:49:43 -0400

    first push

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+//telling express to use hbs as the view engine
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log);
+    //allows the server to make a log file
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+//static takes the absolute path to the folder you want to serve up
+//__dirname variable stores the path to your project's directory
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeText: 'Welcome to the home page!',
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send('Error handling request');
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
