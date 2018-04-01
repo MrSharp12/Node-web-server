c9a0cafaf447feeae09d3dd48c4bb4ff0a1256ed
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-04-01 16:17:11 -0400

    added project page

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 //static takes the absolute path to the folder you want to serve up
 //__dirname variable stores the path to your project's directory
@@ -51,6 +51,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
     });
 });
 
